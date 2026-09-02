d0e164a708b8e650e20af9b711659ca01a8b9f2d
Maria Gabriela <mgabrielamnts@gmail.com>
2026-09-02 20:13:18 -0300

    Adiciona funcionalidades basicas a calculadora

--- App.js
+++ App.js
@@ -1,14 +1,94 @@
 import { StatusBar } from "expo-status-bar";
 import {StyleSheet, Text, View, TouchableOpacity} from "react-native";
+import React, { useState } from "react";
+
+
 
 function App() {
+  //variáveis
+  const [valor, setValor] = useState("0");
+  const [valorGuardado, setValorGuardado] = useState(null);
+  const [operacao, setOperacao] = useState(null);
+  const [expressao, setExpressao] = useState("");
+
+
+  //Limitando número de caracteres no visor para 9 caracteres.
+  const adicionarNumero = (numero) => {
+    if (valor.length <10){
+      setValor(valor === "0" ? numero : valor + numero);
+      setExpressao(expressao + numero);
+    }
+  };
+
+  //Adionando ponto "." nos números
+  const adicionaPonto = () => {
+    if (!valor.includes(".") && valor.length < 10) {
+      setValor(valor + ".")
+    };
+  }
+
+  //Botão de limpeza um a um - DEL
+  const apagarValoresDel = () => {
+    if (valor.length === 1) {
+      setValor("0");
+      setExpressao("");}
+    else{
+      setValor(valor.slice(0,-1));
+      setExpressao(expressao.slice(0,-1));
+    }
+    };
+
+  // Botão limpeza total - AC
+  const limparTotal = () => {
+    setValor("0");
+    setExpressao("");
+  };
+
+  //Botão troca sinal - +/-
+  const trocarSinal = () => {
+    if (valor !== "0"){
+      if(valor.startsWith("-")){
+        setValor(valor.slice(1));}
+      else {
+        setValor("-" + valor);
+      }
+      }
+    };
+
+  //Botão de Porcentagem
+  const calcularPorcentagem = () => {
+    setValor (String(Number(valor/100)))
+  }
+
+  //Expressão do cálculo aparece no visor
+  const selecionarOperacao = (tipoOperacao) => {
+    setValorGuardado(valor);
+    setOperacao(tipoOperacao);
+    setExpressao(expressao + tipoOperacao)
+    setValor("0");
+  };
+
+  //Calculando resultados
+  const calcularResultado = () => {
+    const numero1 = Number(valorGuardado);
+    const numero2 = Number(valor);
+
+    if(operacao === "+"){
+      const resultado = numero1 + numero2;
+      setValor(String(resultado));
+      setExpressao(String(resultado));
+    };
+  }
+
+
+
   return (
     <View style={styles.container}>
 
       <StatusBar style="light" />
       <Text style={styles.titulo}>Calculadora da Bibi ♡</Text>
 
-      <Text style={styles.painel}>0 </Text>
+      <Text style={styles.painel}>{expressao || valor}</Text>
 
 
       <View style={styles.colunas}>
@@ -40,19 +120,23 @@ function App() {
 
       <View style={styles.colunas}>
 
-        <TouchableOpacity style={styles.botaoEspecial}>
+        <TouchableOpacity style={styles.botaoEspecial}
+        onPress={limparTotal}>
           <Text style={styles.textoEspecial}>AC</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoEspecial}>
-          <Text style={styles.textoEspecial}>C</Text>
+        <TouchableOpacity style={styles.botaoEspecial}
+        onPress={apagarValoresDel}>
+          <Text style={styles.textoEspecial}>DEL</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoEspecial}>
+        <TouchableOpacity style={styles.botaoEspecial}
+        onPress={trocarSinal}>
           <Text style={styles.textoEspecial}>+/-</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoEspecial}>
+        <TouchableOpacity style={styles.botaoEspecial}
+        onPress={calcularPorcentagem}>
           <Text style={styles.textoEspecial}>%</Text>
         </TouchableOpacity>
 
@@ -63,15 +147,21 @@ function App() {
 
       <View style={styles.colunas}>
 
-        <TouchableOpacity style={styles.botaoNumero}>
-          <Text style={styles.textoNumeros}>7</Text>
+        <TouchableOpacity style={styles.botaoNumero}
+         onPress={() => adicionarNumero("7")}>
+         <Text style={styles.textoNumeros}>7</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+
+
+
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("8")}>
           <Text style={styles.textoNumeros}>8</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("9")}>
           <Text style={styles.textoNumeros}>9</Text>
         </TouchableOpacity>
 
@@ -86,15 +176,18 @@ function App() {
 
       <View style={styles.colunas}>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("4")}>
           <Text style={styles.textoNumeros}>4</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("5")}>
           <Text style={styles.textoNumeros}>5</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("6")}>
           <Text style={styles.textoNumeros}>6</Text>
         </TouchableOpacity>
 
@@ -109,15 +202,18 @@ function App() {
 
       <View style={styles.colunas}>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("1")}>
           <Text style={styles.textoNumeros}>1</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("2")}>
           <Text style={styles.textoNumeros}>2</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("3")}>
           <Text style={styles.textoNumeros}>3</Text>
         </TouchableOpacity>
 
@@ -132,19 +228,23 @@ function App() {
 
       <View style={styles.colunas}>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={() => adicionarNumero("0")}>
           <Text style={styles.textoNumeros}>0</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoNumero}>
+        <TouchableOpacity style={styles.botaoNumero}
+        onPress={adicionaPonto}>
           <Text style={styles.textoNumeros}>.</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoIgual}>
+        <TouchableOpacity style={styles.botaoIgual}
+        onPress={calcularResultado}>
           <Text style={styles.textoOperacao}>=</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botaoOperacao}>
+        <TouchableOpacity style={styles.botaoOperacao}
+        onPress={() => selecionarOperacao ("+")}>
           <Text style={styles.textoOperacao}>+</Text>
         </TouchableOpacity>
 
